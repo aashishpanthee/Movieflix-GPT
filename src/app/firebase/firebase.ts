8d0379aae2f73cb8b9c1910d3cbb3f4efe29d9a7
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-18 22:33:22 +0545

    refactor: import getAuth from firebase

--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { CONFIG } from '../config/index.config';
+import { getAuth } from 'firebase/auth';
 
 // Initialize Firebase
 const app = initializeApp(CONFIG.firebasConfig);
 export default app;
+
+export const auth = getAuth();
