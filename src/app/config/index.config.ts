584951b54584cdd02e32f2e6b189c776e7dc3b69
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-20 17:47:54 +0545

    add: tmdb api key

--- src/app/config/index.config.ts
+++ src/app/config/index.config.ts
@@ -11,7 +11,7 @@ export const CONFIG: ConfigType = {
   tmdbApiConfig: {
     headers: {
       accept: 'application/json',
-      Authorization: ` Bearer ${process.env.REACT_APP_TMDB_API_KEY ?? ''}`,
+      Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_READ_ACCESS_TOKEN}`,
     },
   },
 };
