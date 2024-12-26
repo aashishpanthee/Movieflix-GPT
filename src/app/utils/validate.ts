00c766baf3b54597bb14799985cef8605652552d
Aashish Panthee <aashishpanthee060@gmail.com>
2024-12-26 15:50:44 +0545

    refactor: extracted validating email & password regex to separate function

--- src/app/utils/validate.ts
+++ src/app/utils/validate.ts
@@ -10,19 +10,23 @@ export const validateUserCredentials = (
   if (!isSignIn && name && name.trim().length < 5) {
     return 'Name must contain at least 5 characters';
   }
-
-  const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(
-    email
-  );
-  const isPasswordValid =
-    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
-      password
-    );
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid = validatePassword(password);
   if (!isEmailValid) {
-    return 'Invalid email address';
+    return 'Invalid email address.';
   }
   if (!isPasswordValid) {
     return 'Password must contain at least 8 characters, including uppercase, lowercase, number, and special character';
   }
   return null;
 };
+
+function validateEmail(email: string): boolean {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+function validatePassword(password: string): boolean {
+  const regex =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  return regex.test(password);
+}
